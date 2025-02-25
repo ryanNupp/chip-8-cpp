8bce8d5835bb9bbc47afb8b97937afe4e744f952
ryanNupp <ryan.nuppenau@icloud.com>
2025-02-25 01:58:58 -0600

    additions

---
+++ build/src/CMakeFiles/chip-8-cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chip-8-cpp.dir/link.d"
+  "CMakeFiles/chip-8-cpp.dir/main.cc.o"
+  "CMakeFiles/chip-8-cpp.dir/main.cc.o.d"
+  "chip-8-cpp"
+  "chip-8-cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chip-8-cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
